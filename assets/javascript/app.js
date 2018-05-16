f2a5cfdf7898e3603f3b51c74f724a794f8c11c5
Raymond <rs01247@gmail.com>
2018-05-15 21:22:20 -0700

    started writing questions and responses

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,189 +9,120 @@ window.onload = function () {
 
 
     // PLACE QUESTIONS IN ARRAY TO CLEAN UP
-    var q1 = {
-        title: "Question 1",
-        ask: "What gift does Johnny present to Lisa in the opening scene?",
-        answer1: "Black Heels",
-        answer2: "Pearl Necklace",
-        answer3: "Red Dress",
-        answer4: "Diamond Ring",
-        correctAnswer: "Red Dress",
-        imageUrl: "",
-    };
-
-    var q2 = {
-        title: "Question 2",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q3 = {
-        title: "Question 3",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q4 = {
-        title: "Question 4",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q5 = {
-        title: "Question 5",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q6 = {
-        title: "Question 6",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q7 = {
-        title: "Question 7",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q8 = {
-        title: "Question 8",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q9 = {
-        title: "Question 9",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
-    var q10 = {
-        title: "Question 10",
-        ask: "",
-        answer1: "",
-        answer2: "",
-        answer3: "",
-        answer4: "",
-        correctAnswer: "",
-        imageUrl: "",
-    };
-
 
     //CHANGE THIS INTO AN ARRAY OF OBJECTS [ { } ]. SWITCH THE CURLY TO BRACKETS
-    var questions = {
-        ask: [
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-        ],
+    var questions = [
 
-        response: [
-            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },
-            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },            {
-                answer1: "",
-                answer2: "",
-                answer3: "",
-                answer4: "",
-            },
-        ]
-    };
+        q1 = {
+            title: "Question 1",
+            ask: "What gift does Johnny present to Lisa in the opening scene?",
+            answer1: "Black Heels",
+            answer2: "Pearl Necklace",
+            answer3: "Red Dress",
+            answer4: "Diamond Ring",
+            correctAnswer: "Red Dress",
+            imageUrl: "",
+        },
 
-    var questions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
+        q2 = {
+            title: "Question 2",
+            ask: "What are the characters always throwing around?",
+            answer1: "Footballs",
+            answer2: "Their Backs",
+            answer3: "Spoons",
+            answer4: "Paper Airplanes",
+            correctAnswer: "Footballs",
+            imageUrl: "",
+        },
+
+        q3 = {
+            title: "Question 3",
+            ask: "Who is Johnny's best friend?",
+            answer1: "Alan",
+            answer2: "Greg",
+            answer3: "Tommy",
+            answer4: "Barney",
+            correctAnswer: "Greg",
+            imageUrl: "",
+        },
+
+        q4 = {
+            title: "Question 4",
+            ask: "What does Lisa's mom suddenly announce?",
+            answer1: "",
+            answer2: "Moving to a new town",
+            answer3: "Bought a new car",
+            answer4: "She has breast cancer",
+            correctAnswer: "She has breast cancer",
+            imageUrl: "",
+        },
+
+        q5 = {
+            title: "Question 5",
+            ask: "What city is The Room based in?",
+            answer1: "Los Angeles",
+            answer2: "Boston",
+            answer3: "Las Vegas",
+            answer4: "San Francisco",
+            correctAnswer: "San Francisco",
+            imageUrl: "",
+        },
+
+        q6 = {
+            title: "Question 6",
+            ask: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            correctAnswer: "",
+            imageUrl: "",
+        },
+
+        q7 = {
+            title: "Question 7",
+            ask: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            correctAnswer: "",
+            imageUrl: "",
+        },
+
+        q8 = {
+            title: "Question 8",
+            ask: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            correctAnswer: "",
+            imageUrl: "",
+        },
+
+        q9 = {
+            title: "Question 9",
+            ask: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            correctAnswer: "",
+            imageUrl: "",
+        },
+
+        q10 = {
+            title: "Question 10",
+            ask: "",
+            answer1: "",
+            answer2: "",
+            answer3: "",
+            answer4: "",
+            correctAnswer: "",
+            imageUrl: "",
+        },
+    ];
 
     function init() {
         $("#main-content").show();
